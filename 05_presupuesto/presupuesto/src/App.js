c1f5b89d7f5769f6ae15069eec5eca9c423ca88c
antoniofd20 <antonioflodiaz@gmail.com>
2020-10-13 19:52:45 -0500

    presupuesto, formulario

--- 05_presupuesto/presupuesto/src/App.js
+++ 05_presupuesto/presupuesto/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Pregunta from './components/Pregunta';
+import Formulario from './components/Formulario';
 
 function App() {
+
+  // Definir el state
+  const [presupuesto, guardarPresupuesto ] = useState(0);
+  const [ restante, guardarResrante ] = useState(0);
+  const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className = "container">
+      <header>
+        <h1>Gasto semanal</h1>
+
+        <div className = "contenido-principal contenido">
+
+        { mostrarpregunta ? (
+            <Pregunta
+              guardarPresupuesto = {guardarPresupuesto}
+              guardarResrante = {guardarResrante}
+              actualizarPregunta = {actualizarPregunta}
+            />
+          ) : (
+            <div className = "row">
+              <div className = "one-half column">
+                <Formulario/>
+              </div>
+              <div className = "one-half column">
+                2
+              </div>
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+/** Proximo video #10 carpeta 6
+ *  link https://mega.nz/folder/NigAnKaB#2uGl78a19MRo6zSnkt2HoA/folder/1v4XkKST
+ */
---
+++ 05_presupuesto/presupuesto/src/components/Pregunta.jsx
@@ -0,0 +1,58 @@
+import React, { Fragment, useState } from 'react'
+import Error from './Error';
+
+const Pregunta = ({guardarPresupuesto, guardarResrante, actualizarPregunta}) => {
+    
+    // Definir el state
+    const [ cantidad, guardarCantidad ] = useState(0);
+    const [ error, guardarError ] = useState(false);
+
+    //Funcion que lee el presupuesto
+    const definirPresupuesto = e => {
+        guardarCantidad(parseInt(e.target.value, 10));
+    }
+
+    // Submit para definir el presupuesto
+    const agregarPresupuesto = e => {
+        // Para que no refresque la página
+        e.preventDefault();
+
+        // validar 
+        if(cantidad < 1 || isNaN(cantidad)){
+            guardarError(true);
+            return;
+        }
+        // Si pasa la validación
+        guardarError(false);
+        guardarPresupuesto(cantidad);
+        guardarResrante(cantidad);
+        actualizarPregunta(false);
+    }
+
+    return (
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+
+            { error ?  <Error mensaje = "El presupuesto es incorrecto"/> : null }
+
+            <form
+                onSubmit = {agregarPresupuesto}
+            >
+                <input
+                    type = "number"
+                    className = "u-full-width"
+                    placeholder = "Coloca tu presupuesto"
+                    onChange = {definirPresupuesto}
+                />
+
+                <input
+                    type = "submit"
+                    className = "button-primary u-full-width"
+                    value = "Definir presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+}
+ 
+export default Pregunta;
